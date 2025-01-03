4ef0dc6910714f28644eda158df5a899db2e4c47
Yoshita-tech <yoshita027@gmail.com>
2025-01-03 17:55:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Hello all"
+  return "Hello all ${BUILD_NUMBER}"
 
 if __name__ == '__main__':
   port=int(os.environ.get("PORT",5000))
